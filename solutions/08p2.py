d4ac075bea676aa7ea017b198988e3027e64c927
japiirainen <joona.piirainen@gmail.com>
2025-12-08 09:17:35 +0200

    feat(08): solve part 2

---
+++ solutions/08p2.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+lines = open(0).read().splitlines()
+
+points = [tuple(map(int, line.split(","))) for line in lines]
+
+dists = sorted(
+    ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2, i, j)
+    for i, (x1, y1, z1) in enumerate(points)
+    for j, (x2, y2, z2) in enumerate(points)
+    if i > j
+)
+
+
+uf = {i: i for i in range(len(points))}
+
+
+def find(x: int) -> int:
+    if x == uf[x]:
+        return x
+    uf[x] = find(uf[x])
+    return uf[x]
+
+
+def mix(x, y) -> None:
+    uf[find(x)] = uf[y]
+
+
+conns = 0
+
+for t, (_d, i, j) in enumerate(dists):
+    if t == 1000:
+        sz = defaultdict(int)
+        for x in range(len(points)):
+            sz[find(x)] += 1
+        s = sorted(sz.values())
+    if find(i) != find(j):
+        conns += 1
+        if conns == len(points) - 1:
+            print(points[i][0] * points[j][0])
+        mix(i, j)

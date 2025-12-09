2dcfe7f3bd873eff4399c4857dfc4a7eae8efc2f
japiirainen <joona.piirainen@gmail.com>
2025-12-09 07:34:33 +0200

    feat(08): simplify part 1 solution

--- solutions/08p1.py
+++ solutions/08p1.py
@@ -1,40 +1,36 @@
-from collections import defaultdict
+import math
 
 lines = open(0).read().splitlines()
 
 points = [tuple(map(int, line.split(","))) for line in lines]
 
-dists = sorted(
-    ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2, i, j)
-    for i, (x1, y1, z1) in enumerate(points)
-    for j, (x2, y2, z2) in enumerate(points)
-    if i > j
+edges = sorted(
+    [(i, j) for i in range(len(points)) for j in range(i + 1, len(points))],
+    key=lambda x: math.hypot(*[a - b for a, b in zip(points[x[0]], points[x[1]])]),
 )
 
+parent = list(range(len(points)))
 
-uf = {i: i for i in range(len(points))}
 
-
-def find(x: int) -> int:
-    if x == uf[x]:
+def root(x):
+    if parent[x] == x:
         return x
-    uf[x] = find(uf[x])
-    return uf[x]
+    parent[x] = root(parent[x])
+    return parent[x]
 
 
-def mix(x, y) -> None:
-    uf[find(x)] = uf[y]
+def merge(a, b):
+    parent[root(a)] = root(b)
 
 
-conns = 0
+for a, b in edges[:1000]:
+    merge(a, b)
 
-for t, (_d, i, j) in enumerate(dists):
-    if t == 1000:
-        sz = defaultdict(int)
-        for x in range(len(points)):
-            sz[find(x)] += 1
-        s = sorted(sz.values())
-        print(s[-1] * s[-2] * s[-3])
-    if find(i) != find(j):
-        conns += 1
-        mix(i, j)
+sizes = [0] * len(points)
+
+for point in range(len(points)):
+    sizes[root(point)] += 1
+
+sizes.sort(reverse=True)
+
+print(sizes[0] * sizes[1] * sizes[2])

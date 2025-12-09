ef2951db529c17617553de1d195e3d493fe724a0
japiirainen <joona.piirainen@gmail.com>
2025-12-09 07:34:45 +0200

    feat(08): simplify part 2 solution

--- solutions/08p2.py
+++ solutions/08p2.py
@@ -1,41 +1,35 @@
-from collections import defaultdict
+import math
 
 lines = open(0).read().splitlines()
 
 points = [tuple(map(int, line.split(","))) for line in lines]
 
-dists = sorted(
-    ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2, i, j)
-    for i, (x1, y1, z1) in enumerate(points)
-    for j, (x2, y2, z2) in enumerate(points)
-    if i > j
+edges = sorted(
+    [(i, j) for i in range(len(points)) for j in range(i + 1, len(points))],
+    key=lambda x: math.hypot(*[a - b for a, b in zip(points[x[0]], points[x[1]])]),
 )
 
+parent = list(range(len(points)))
 
-uf = {i: i for i in range(len(points))}
 
-
-def find(x: int) -> int:
-    if x == uf[x]:
+def root(x):
+    if parent[x] == x:
         return x
-    uf[x] = find(uf[x])
-    return uf[x]
+    parent[x] = root(parent[x])
+    return parent[x]
 
 
-def mix(x, y) -> None:
-    uf[find(x)] = uf[y]
+def merge(a, b):
+    parent[root(a)] = root(b)
 
 
-conns = 0
+circuits = len(points)
 
-for t, (_d, i, j) in enumerate(dists):
-    if t == 1000:
-        sz = defaultdict(int)
-        for x in range(len(points)):
-            sz[find(x)] += 1
-        s = sorted(sz.values())
-    if find(i) != find(j):
-        conns += 1
-        if conns == len(points) - 1:
-            print(points[i][0] * points[j][0])
-        mix(i, j)
+for a, b in edges:
+    if root(a) == root(b):
+        continue
+    merge(a, b)
+    circuits -= 1
+    if circuits == 1:
+        print(points[a][0] * points[b][0])
+        break

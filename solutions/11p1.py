3081b6a8b623e7aadf4aebd67477e47e4266d589
japiirainen <joona.piirainen@gmail.com>
2025-12-12 09:50:14 +0200

    feat(11): solve part 1

---
+++ solutions/11p1.py
@@ -0,0 +1,11 @@
+from functools import cache
+
+G = {k[:-1]: v for k, *v in map(str.split, open(0))} | {"out": []}
+
+
+@cache
+def count(here, there):
+    return here == there or sum(count(n, there) for n in G[here])
+
+
+print(count("you", "out"))
